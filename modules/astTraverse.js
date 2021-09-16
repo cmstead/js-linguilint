ef19bfd40dad78e7ecb91e7d470993ee0d199af2
Chris Stead <cmstead@gmail.com>
2021-09-15 22:21:54 -0700

    F Added ast traversal infrastructure

---
+++ modules/astTraverse.js
@@ -0,0 +1,29 @@
+const estraverse = require('estraverse');
+const { JSX_ELEMENT, JSX_EXPRESSION_CONTAINER } = require('./ast-node-types ');
+const { getNodeType } = require('./core-utils');
+
+function traverse(ast, behaviors) {
+    return estraverse.traverse(ast, {
+        ...behaviors,
+        fallback(node) {
+            const nodeType = getNodeType(node);
+
+            if (nodeType.startsWith('TS')) {
+                return estraverse.VisitorOption.Skip;
+            } else if(nodeType === JSX_ELEMENT) {
+                node.children.forEach(node => {
+                    traverse(node, behaviors);
+                });
+            } else if(nodeType === JSX_EXPRESSION_CONTAINER) {
+                traverse(node.expression, behaviors);
+            }
+
+            return node;
+        }
+    });
+}
+
+module.exports = {
+    traverse,
+    VisitorOption: estraverse.VisitorOption
+};
